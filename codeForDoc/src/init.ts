cd2cc9e83bf9bf32fae978681d5caf693fbd8711
Acid Coder <5227509+tylim88@users.noreply.github.com>
2022-09-04 05:08:27 +0800

    Update init.ts

--- codeForDoc/src/init.ts
+++ codeForDoc/src/init.ts
@@ -10,7 +10,7 @@ const app = initializeApp({
 	credential: cert(serviceAccount as ServiceAccount),
 })
 
-export const db = getFirestore() // or getFirestore(app), you can also skip this line
+export const db = getFirestore() // or getFirestore(app), you can skip this line
 
 // Recommendation: Export this if the collection is sub-collection and then fill in collection path later, because sub collection most likely have dynamic document ID.
 export const firelordExample = getFirelord<Example>() // or getFirelord<Example>(db)
