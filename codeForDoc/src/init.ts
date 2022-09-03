64971b9f01a7c58bd62c505b0a6f09aa09b0bad8
Acid Coder <5227509+tylim88@users.noreply.github.com>
2022-09-04 05:10:25 +0800

    Update init.ts

--- codeForDoc/src/init.ts
+++ codeForDoc/src/init.ts
@@ -1,16 +1,7 @@
-import { getFirelord, getFirestore } from 'firelord'
-import { initializeApp, cert, ServiceAccount } from 'firebase-admin/app'
+import { getFirelord, getFirestore } from 'firelordrn'
 import { Example } from './dataType'
-// if you run in cloud function
-const app = initializeApp()
 
-// if you run in custom backend or you run in cloud function but want to access other project firestore
-import serviceAccount from './serviceAccount.json' // get it from firebase console --> project setting --> service accounts, THIS IS A SECERT!
-const app = initializeApp({
-	credential: cert(serviceAccount as ServiceAccount),
-})
-
-export const db = getFirestore() // or getFirestore(app), you can skip this line
+export const db = getFirestore() // you can skip this line
 
 // Recommendation: Export this if the collection is sub-collection and then fill in collection path later, because sub collection most likely have dynamic document ID.
 export const firelordExample = getFirelord<Example>() // or getFirelord<Example>(db)
